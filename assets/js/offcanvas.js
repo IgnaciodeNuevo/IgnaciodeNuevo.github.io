250202bd37a686075697da2663ce7185a932042a
IgnaciodeNuevo <ignaciodenuevo@posteo.net>
2019-03-08 19:47:09 +0100

    Updated offcanvas constants to uppercase

--- assets/js/offcanvas.js
+++ assets/js/offcanvas.js
@@ -1,22 +1,22 @@
 (function () {
-    const escapeCode = 27;
-    const navOpenButton = document.querySelector('#js-open-menu');
-    const navCloseButton = document.querySelector('#js-close-menu');
-    const navMenu = document.querySelector('#js-nav');
-    const navLinks = navMenu.getElementsByTagName('a');
+    const ESCAPE_CODE = 27;
+    const NAV_OPEN_BUTTON = document.querySelector('#js-open-menu');
+    const NAV_CLOSE_BUTTON = document.querySelector('#js-close-menu');
+    const NAV_MENU = document.querySelector('#js-nav');
+    const NAV_LINKS = NAV_MENU.getElementsByTagName('a');
 
     function initApp() {
-        navMenu.addEventListener('keydown', handleKeydown);
-        navOpenButton.addEventListener('click', handleClick);
-        navCloseButton.addEventListener('click', handleClick);
+        NAV_MENU.addEventListener('keydown', handleKeydown);
+        NAV_OPEN_BUTTON.addEventListener('click', handleClick);
+        NAV_CLOSE_BUTTON.addEventListener('click', handleClick);
         disableNavLinks();
     }
 
     function handleKeydown(event) {
-        if (event.keyCode === escapeCode) {
+        if (event.keyCode === ESCAPE_CODE) {
             document.body.classList.remove('is-active');
             disableNavLinks();
-            navOpenButton.focus();
+            NAV_OPEN_BUTTON.focus();
         }
     }
 
@@ -27,36 +27,36 @@
         } else {
             document.body.classList.add('is-active');
             enableNavLinks();
-            navLinks[0].focus();
+            NAV_LINKS[0].focus();
         }
     }
 
     function enableNavLinks() {
-        navOpenButton.setAttribute('aria-label', 'Menu expanded');
-        navCloseButton.setAttribute('aria-label', 'Menu expanded');
-        navCloseButton.removeAttribute('tabIndex');
-        navMenu.removeAttribute('aria-hidden');
-        for (let i = 0; i < navLinks.length; ++i) {
-            navLinks[i].removeAttribute('tabIndex');
+        NAV_OPEN_BUTTON.setAttribute('aria-label', 'Menu expanded');
+        NAV_CLOSE_BUTTON.setAttribute('aria-label', 'Menu expanded');
+        NAV_CLOSE_BUTTON.removeAttribute('tabIndex');
+        NAV_MENU.removeAttribute('aria-hidden');
+        for (let i = 0; i < NAV_LINKS.length; ++i) {
+            NAV_LINKS[i].removeAttribute('tabIndex');
         }
     }
 
     function disableNavLinks() {
-        navOpenButton.setAttribute('aria-label', 'Menu collapsed');
-        navCloseButton.setAttribute('aria-label', 'Menu collapsed');
-        navCloseButton.tabIndex = -1;
-        navMenu.setAttribute('aria-hidden', 'true');
-        for (let i = 0; i < navLinks.length; ++i) {
-            navLinks[i].tabIndex = -1;
+        NAV_OPEN_BUTTON.setAttribute('aria-label', 'Menu collapsed');
+        NAV_CLOSE_BUTTON.setAttribute('aria-label', 'Menu collapsed');
+        NAV_CLOSE_BUTTON.tabIndex = -1;
+        NAV_MENU.setAttribute('aria-hidden', 'true');
+        for (let i = 0; i < NAV_LINKS.length; ++i) {
+            NAV_LINKS[i].tabIndex = -1;
         }
     }
 
     initApp();
 
     window.addEventListener('resize', function () {
-        const activeClassName = 'is-active';
-        if (window.innerWidth > 768 && document.body.classList.contains(activeClassName)) {
-            document.body.classList.remove(activeClassName);
+        const ACTIVE_CLASS_NAME = 'is-active';
+        if (window.innerWidth > 768 && document.body.classList.contains(ACTIVE_CLASS_NAME)) {
+            document.body.classList.remove(ACTIVE_CLASS_NAME);
             disableNavLinks();
         }
     });
