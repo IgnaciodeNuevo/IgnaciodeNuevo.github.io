0531a5df27e7f1c47773480391440b3987fc9283
IgnaciodeNuevo <ignaciodenuevo@posteo.net>
2019-04-13 22:26:03 +0200

    Small refactor and added trapFocus() function

--- assets/js/offcanvas.js
+++ assets/js/offcanvas.js
@@ -2,8 +2,10 @@
     const navOpenButton = document.querySelector('#js-open-menu');
     const navCloseButton = document.querySelector('#js-close-menu');
     const navMenu = document.querySelector('#js-nav');
-    const navLinks = navMenu.getElementsByTagName('a');
-    const escapeCode = 27;
+    const navLinks = navMenu.querySelectorAll('.link');
+    const focusableElements = navMenu.querySelectorAll('.link, #js-close-menu');
+    const firstFocusableElement = focusableElements[0];
+    const lastFocusableElement = focusableElements[focusableElements.length - 1];
     const activeClassName = 'is-active';
 
     function initApp() {
@@ -13,32 +15,36 @@
         disableNavLinks();
     }
 
+    function removeBodyActiveClass() {
+        document.body.classList.remove(activeClassName);
+    }
+
     function handleKeydown(event) {
-        if (event.keyCode === escapeCode) {
-            document.body.classList.remove('is-active');
+        const isEscapePressed = event.key === 'Escape' || event.keyCode === 27;
+
+        if (isEscapePressed) {
+            removeBodyActiveClass()
             disableNavLinks();
             navOpenButton.focus();
         }
     }
 
     function handleClick() {
-        if (document.body.classList.contains('is-active')) {
-            document.body.classList.remove('is-active');
+        if (document.body.classList.contains(activeClassName)) {
+            removeBodyActiveClass()
             disableNavLinks();
         } else {
-            document.body.classList.add('is-active');
-            enableNavLinks();
-            navLinks[0].focus();
-        }
-    }
+            document.body.classList.add(activeClassName);
+            navOpenButton.setAttribute('aria-label', 'Menu expanded');
+            navCloseButton.setAttribute('aria-label', 'Menu expanded');
+            navCloseButton.removeAttribute('tabIndex');
+            navMenu.removeAttribute('aria-hidden');
 
-    function enableNavLinks() {
-        navOpenButton.setAttribute('aria-label', 'Menu expanded');
-        navCloseButton.setAttribute('aria-label', 'Menu expanded');
-        navCloseButton.removeAttribute('tabIndex');
-        navMenu.removeAttribute('aria-hidden');
-        for (let i = 0; i < navLinks.length; ++i) {
-            navLinks[i].removeAttribute('tabIndex');
+            for (let i = 0; i < navLinks.length; ++i) {
+                navLinks[i].removeAttribute('tabIndex');
+            }
+
+            navLinks[0].focus();
         }
     }
 
@@ -47,16 +53,43 @@
         navCloseButton.setAttribute('aria-label', 'Menu collapsed');
         navCloseButton.tabIndex = -1;
         navMenu.setAttribute('aria-hidden', 'true');
+
         for (let i = 0; i < navLinks.length; ++i) {
             navLinks[i].tabIndex = -1;
         }
     }
 
+    function trapFocus() {
+        console.log('focusableElements :', focusableElements);
+        navMenu.addEventListener('keydown', function (event) {
+            const isTabPressed = event.key === 'Tab' || event.keyCode === 9;
+
+            if (!isTabPressed) {
+                return;
+            }
+
+            if (event.shiftKey) {
+                /* Shift + Tab */
+                if (document.activeElement === firstFocusableElement) {
+                    lastFocusableElement.focus();
+                    event.preventDefault();
+                }
+            } else {
+                /* Tab */
+                if (document.activeElement === lastFocusableElement) {
+                    firstFocusableElement.focus();
+                    event.preventDefault();
+                }
+            }
+        });
+    }
+
     initApp();
+    trapFocus();
 
     window.matchMedia('(min-width: 48em)').addListener(function () {
         if (document.body.classList.contains(activeClassName)) {
-            document.body.classList.remove(activeClassName);
+            removeBodyActiveClass()
             disableNavLinks();
         }
     });
