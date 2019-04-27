583376be3d8f043e7287dd4d8de4e13466f30deb
IgnaciodeNuevo <ignaciodenuevo@posteo.net>
2019-04-27 18:08:34 +0200

    Updated const name from isScrollPast500px to hasScrollPast500px

--- assets/js/scroll-to-top.js
+++ assets/js/scroll-to-top.js
@@ -1,9 +1,9 @@
 window.addEventListener('scroll', function () {
     // When the user scrolls down 500px from the top of the document, show the button
-    const isScrollPast500px = document.body.scrollTop > 500 || document.documentElement.scrollTop > 500;
+    const hasScrollPast500px = document.body.scrollTop > 500 || document.documentElement.scrollTop > 500;
     const topBtn = document.getElementById('topBtn');
 
-    if (isScrollPast500px) {
+    if (hasScrollPast500px) {
         topBtn.style.display = 'block';
     } else {
         topBtn.style.display = 'none';
