818549bab2ff257b67f2acbaad822b15700a425e
IgnaciodeNuevo <ignaciodenuevo@posteo.net>
2019-04-27 15:03:33 +0200

    Updated scrikk-to-top.js comment

--- assets/js/scroll-to-top.js
+++ assets/js/scroll-to-top.js
@@ -1,4 +1,4 @@
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 500px from the top of the document, show the button
 window.addEventListener('scroll', function () {
     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
         document.getElementById('topBtn').style.display = 'block';
