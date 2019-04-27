b6ebb6720bb5b83062c50b9cadcb442641f50a72
IgnaciodeNuevo <ignaciodenuevo@posteo.net>
2019-04-27 16:03:54 +0200

    Added event.preventDefault();

--- assets/js/scroll-to-top.js
+++ assets/js/scroll-to-top.js
@@ -1,5 +1,7 @@
 // When the user scrolls down 500px from the top of the document, show the button
-window.addEventListener('scroll', function () {
+window.addEventListener('scroll', function(event) {
+    event.preventDefault();
+
     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
         document.getElementById('topBtn').style.display = 'block';
     } else {
