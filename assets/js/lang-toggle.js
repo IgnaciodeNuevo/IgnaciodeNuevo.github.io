1e2d89df572579f0013d955e24c2b14e244a208a
IgnaciodeNuevo <ignaciodenuevo@posteo.net>
2019-05-03 13:59:45 +0200

    Refactored lang-toggle.js

--- assets/js/lang-toggle.js
+++ assets/js/lang-toggle.js
@@ -1,50 +1,46 @@
-const selectAll = document.querySelector('.js-lang-all');
-const selectSpanish = document.querySelector('.js-lang-es');
-const selectEnglish = document.querySelector('.js-lang-en');
+const selectAllButton = document.querySelector('.js-lang-all');
+const selectSpanishButton = document.querySelector('.js-lang-es');
+const selectEnglishButton = document.querySelector('.js-lang-en');
 
 const allArticles = document.querySelectorAll('article');
 const spanishArticles = document.querySelectorAll("[data-lang='es']");
 const englishArticles = document.querySelectorAll("[data-lang='en']");
 
+const hiddenClassName = 'is-hidden';
+
 // Declared in offcanvas.js => const activeClassName = 'is-active';
 
-selectAll.addEventListener('click', () => {
-    selectAll.classList.add(activeClassName);
+const selectButtonToBeActive = (buttonToAddActiveClass, firstButtonToRemoveActiveClass, secondButtonToRemoveActiveClass) => {
+    buttonToAddActiveClass.classList.add(activeClassName);
 
-    selectEnglish.classList.remove(activeClassName);
-    selectSpanish.classList.remove(activeClassName);
+    firstButtonToRemoveActiveClass.classList.remove(activeClassName);
+    secondButtonToRemoveActiveClass.classList.remove(activeClassName);
+};
 
-    allArticles.forEach(element => {
-        element.classList.remove('is-hidden');
+const toggleArticlesHiddenClassName = (articlesToShow, articlesToHide) => {
+    articlesToShow.forEach(article => {
+        article.classList.remove(hiddenClassName);
+    });
+
+    articlesToHide.forEach(article => {
+        article.classList.add(hiddenClassName);
+    });
+}
+
+selectAllButton.addEventListener('click', () => {
+    selectButtonToBeActive(selectAllButton, selectEnglishButton, selectSpanishButton);
+
+    allArticles.forEach(article => {
+        article.classList.remove(hiddenClassName);
     });
 });
 
-selectSpanish.addEventListener('click', () => {
-    selectSpanish.classList.add(activeClassName);
-
-    selectEnglish.classList.remove(activeClassName);
-    selectAll.classList.remove(activeClassName);
-
-    spanishArticles.forEach(element => {
-        element.classList.remove('is-hidden');
-    });
-
-    englishArticles.forEach(element => {
-        element.classList.add('is-hidden');
-    });
+selectSpanishButton.addEventListener('click', () => {
+    selectButtonToBeActive(selectSpanishButton, selectEnglishButton, selectAllButton);
+    toggleArticlesHiddenClassName(spanishArticles, englishArticles);
 });
 
-selectEnglish.addEventListener('click', () => {
-    selectEnglish.classList.add(activeClassName);
-
-    selectSpanish.classList.remove(activeClassName);
-    selectAll.classList.remove(activeClassName);
-
-    englishArticles.forEach(element => {
-        element.classList.remove('is-hidden');
-    });
-
-    spanishArticles.forEach(element => {
-        element.classList.add('is-hidden');
-    });
+selectEnglishButton.addEventListener('click', () => {
+    selectButtonToBeActive(selectEnglishButton, selectSpanishButton, selectAllButton);
+    toggleArticlesHiddenClassName(englishArticles, spanishArticles);
 });
