ba5a558d2b43cbfb1add6f922c590b853edc8750
IgnaciodeNuevo <ignaciodenuevo@posteo.net>
2019-05-06 17:20:25 +0200

    Fixed bug: after opening the offcanvas menu, clicking a section link  and going back the offcanvas is still open

--- assets/js/off-canvas.js
+++ assets/js/off-canvas.js
@@ -60,7 +60,7 @@ function disableNavLinks() {
 }
 
 function trapFocus() {
-    navMenu.addEventListener('keydown', function(event) {
+    navMenu.addEventListener('keydown', function (event) {
         const isTabPressed = event.key === 'Tab' || event.keyCode === 9;
 
         if (!isTabPressed) {
@@ -86,9 +86,13 @@ function trapFocus() {
 initApp();
 trapFocus();
 
-window.matchMedia('(min-width: 48em)').addListener(function() {
+window.matchMedia('(min-width: 48em)').addListener(function () {
     if (document.body.classList.contains(activeClassName)) {
         removeBodyActiveClass()
         disableNavLinks();
     }
+});
+
+window.addEventListener('DOMContentLoaded', function () {
+    removeBodyActiveClass();
 });
